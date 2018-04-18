e441f419176efa62b857eec63b911f526aaabd27
Chronum <jinxhaldar@gmail.com>
2018-04-18 11:35:36 -0400

    Silver spline and data recovery works.

---
+++ Matlab/ag_spline.m
@@ -0,0 +1,7 @@
+function [nspline] = ag_spline(wavelengths)
+    agfile = dlmread('Materials/Ag.txt');
+    % [wv, n, k] = tdfread('Materials/GaAs.txt');
+    agnk = agfile(:, 2) + 1j * agfile(:, 3);
+
+    nspline = interp1(agfile(:, 1), agnk, wavelengths, 'cubic');
+end
