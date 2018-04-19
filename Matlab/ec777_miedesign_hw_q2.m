2853ca41f5b212fb409862f028d6ee81509a0b0a
Chronum <jinxhaldar@gmail.com>
2018-04-19 02:53:28 -0400

    Fields appear to work.

--- Matlab/ec777_miedesign_hw_q2.m
+++ Matlab/ec777_miedesign_hw_q2.m
@@ -1,9 +1,9 @@
 clearvars; close all;
-wv = linspace(0.4, 1.4, 500);
+wv = linspace(0.2, 1.4, 500);
 nmodes = 20;
 
 %% GaAs section
-gaasnk= gaas_spline(wv);
+agnk= ag_spline(wv);
 diams = linspace(0.01, 0.5, 6);
 
 figure(1)
@@ -11,8 +11,8 @@ set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
 plotnum = 1;
 for d=diams
     x = 2*pi./wv * d/2;
-    scqtm_gaas = scatter_q_tm(gaasnk, x, nmodes);
-    scqte_gaas = scatter_q_te(gaasnk, x, nmodes);
+    scqtm_gaas = scatter_q_tm(agnk, x, nmodes);
+    scqte_gaas = scatter_q_te(agnk, x, nmodes);
     subplot(230+plotnum);
     plot(wv, scqtm_gaas, wv ,scqte_gaas);
     ylabel('$Q_{sca}$', 'Interpreter', 'latex');
@@ -29,10 +29,10 @@ set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
 plotnum = 1;
 for d=diams
     x = 2*pi./wv * d/2;
-    scqtm_gaas = scatter_q_tm(gaasnk, x, nmodes);
-    exqtm_gaas = extinct_q_tm(gaasnk, x, nmodes);
-    scqte_gaas = scatter_q_te(gaasnk, x, nmodes);
-    exqte_gaas = extinct_q_te(gaasnk, x, nmodes);
+    scqtm_gaas = scatter_q_tm(agnk, x, nmodes);
+    exqtm_gaas = extinct_q_tm(agnk, x, nmodes);
+    scqte_gaas = scatter_q_te(agnk, x, nmodes);
+    exqte_gaas = extinct_q_te(agnk, x, nmodes);
     subplot(230+plotnum);
     plot(wv, exqtm_gaas - scqtm_gaas, wv, exqte_gaas - scqte_gaas);
     ylabel('$Q_{abs}$', 'Interpreter', 'latex');
@@ -45,37 +45,17 @@ for d=diams
 end
 
 
-%% Constant refractive index section
-figure(3)
-set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
-plotnum = 1;
-d = 0.2;
-rixes = linspace(1.5, 3.5, 6);
-for n=rixes
-    x = 2*pi./wv * d/2;
-    scqtm_n = scatter_q_tm(n, x, nmodes);
-    scqte_n = scatter_q_te(n, x, nmodes);
-    subplot(230+plotnum);
-    plot(wv, scqtm_n, wv ,scqte_n);
-    ylabel('$Q_{sca}$', 'Interpreter', 'latex');
-    xlabel('Wavelength[$\mu m$]', 'Interpreter', 'latex');
-    rawtitle = sprintf('Refractive index of NW: %0.2f', n);
-    title(rawtitle, 'Interpreter', 'latex');
-    legend('TM', 'TE');
-    %ylim([-1, 7]);
-    plotnum = plotnum + 1;
-end
 %% Si section
 
-sink = si_spline(wv);
+aunk = au_spline(wv);
 
-figure(4);
+figure(3);
 set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
 plotnum = 1;
 for d=diams
     x = 2*pi./wv * d/2;
-    scqtm_si = scatter_q_tm(sink, x, nmodes);
-    scqte_si = scatter_q_te(sink, x, nmodes);
+    scqtm_si = scatter_q_tm(aunk, x, nmodes);
+    scqte_si = scatter_q_te(aunk, x, nmodes);
     subplot(230+plotnum);
     plot(wv, scqtm_si, wv, scqte_si);
     ylabel('$Q_{sca}$', 'Interpreter', 'latex');
@@ -86,15 +66,15 @@ for d=diams
     plotnum = plotnum + 1;
 end
 
-figure(5);
+figure(4);
 set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
 plotnum = 1;
 for d=diams
     x = 2*pi./wv * d/2;
-    scqtm_si = scatter_q_tm(sink, x, nmodes);
-    exqtm_si = extinct_q_tm(sink, x, nmodes);
-    scqte_si = scatter_q_te(sink, x, nmodes);
-    exqte_si = extinct_q_te(sink, x, nmodes);
+    scqtm_si = scatter_q_tm(aunk, x, nmodes);
+    exqtm_si = extinct_q_tm(aunk, x, nmodes);
+    scqte_si = scatter_q_te(aunk, x, nmodes);
+    exqte_si = extinct_q_te(aunk, x, nmodes);
     subplot(230+plotnum);
     plot(wv, exqtm_si - scqtm_si, wv, exqtm_si - scqte_si);
     ylabel('$Q_{abs}$', 'Interpreter', 'latex');
@@ -105,8 +85,7 @@ for d=diams
     %ylim([-1, 7]);
     plotnum = plotnum + 1;
 end
-fprintf('Figure 1: TE/TM scattering cross sections for GaAs nanowires with varying diameters.\n');
-fprintf('Figure 2: TE/TM absorption cross sections for GaAs nanowires with varying diameters.\n');
-fprintf('Figure 3: TE/TM scattering cross sections for constant refractive index nanowires of diameter = 200 nm.\n');
-fprintf('Figure 4: TE/TM scattering cross sections for Si nanowires with varying diameters.\n');
-fprintf('Figure 5: TE/TM absorption cross sections for Si nanowires with varying diameters.\n');
+fprintf('Figure 1: TE/TM scattering cross sections for Ag nanowires with varying diameters.\n');
+fprintf('Figure 2: TE/TM absorption cross sections for Ag nanowires with varying diameters.\n');
+fprintf('Figure 4: TE/TM scattering cross sections for Au nanowires with varying diameters.\n');
+fprintf('Figure 5: TE/TM absorption cross sections for Au nanowires with varying diameters.\n');
